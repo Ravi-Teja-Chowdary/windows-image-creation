b5c1886a6b314c2672ed76cbbc242eeda7df5c0c
Ravi-Teja-Chowdary <97519600+Ravi-Teja-Chowdary@users.noreply.github.com>
2026-04-14 16:03:56 +0530

    Update install_office.ps1

--- install_office.ps1
+++ install_office.ps1
@@ -1,25 +1,19 @@
 $configPath = "C:\install\configuration.xml"
 
-Write-Host "--- STAGE: CREATING ODT CONFIG ---"
-# This XML is specifically designed for the Office Deployment Tool
+Write-Host "--- STAGE: CREATING CONFIG ---"
+# Optimized for the Bootstrapper/Standard Setup
 $xmlContent = @"
 <Configuration>
-  <Add SourcePath="C:\install\OfficeInstall" OfficeClientEdition="64">
-    <Product ID="O365ProPlusRetail">
-      <Language ID="en-us" />
-    </Product>
-  </Add>
-  <Display Level="None" AcceptEULA="TRUE" />
-  <Property Name="AUTOACTIVATE" Value="1" />
-  <Property Name="FORCEAPPSHUTDOWN" Value="TRUE" />
-  <Property Name="SharedComputerLicensing" Value="1" />
+  <Display Level="None" CompletionNotice="No" SuppressModal="Yes" AcceptEula="Yes" />
+  <Setting Id="SETUP_REBOOT" Value="Never" />
+  <Setting Id="REBOOT" Value="ReallySuppress"/>
 </Configuration>
 "@
 $xmlContent | Out-File -FilePath $configPath -Encoding utf8
 
-Write-Host "--- STAGE: STARTING ODT INSTALLATION ---"
-# Note: Using /configure instead of /config
-$process = Start-Process -FilePath ".\OfficeInstall\setup.exe" -ArgumentList "/configure `"$configPath`"" -PassThru -NoNewWindow -Wait
+Write-Host "--- STAGE: STARTING INSTALLATION ---"
+# Using /config for the Bootstrapper setup.exe
+$process = Start-Process -FilePath ".\OfficeInstall\setup.exe" -ArgumentList "/config `"$configPath`"" -PassThru -NoNewWindow -Wait
 
 if ($process.ExitCode -ne 0) {
     Write-Host "--- ERROR: Setup failed with exit code $($process.ExitCode) ---"
